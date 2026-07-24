6eeb0be29d04f694248157d0691635685c8350aa
michealsheehy-alt <michealsheehy@gmail.com>
2026-07-24 15:14:24 -0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-
-const toggle=document.querySelector('.mobile-toggle');
-const nav=document.querySelector('.nav-links');
-if(toggle&&nav){toggle.addEventListener('click',()=>nav.classList.toggle('open'));}
-document.querySelectorAll('.nav-links a').forEach(a=>a.addEventListener('click',()=>nav?.classList.remove('open')));
